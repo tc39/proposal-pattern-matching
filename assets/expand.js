fc186d7ee293c6f6c1382ed8ebf1bc6b736705e7
Jack Works <jackworks@protonmail.com>
2023-10-30 03:00:03 +0800

    Add built-in matchers and class default matcher

---
+++ assets/expand.js
@@ -0,0 +1,43 @@
+const state = {
+  __folded: true,
+  get folded() {
+    try {
+      const v = localStorage.getItem("folded");
+      return v === "true" || v === null;
+    } catch {
+      return this.__folded;
+    }
+  },
+  set folded(val) {
+    this.__folded = val;
+    try {
+      localStorage.setItem("folded", val);
+    } catch {}
+    document.body.classList.toggle("folded", val);
+  },
+};
+state.folded = state.folded;
+
+function resetHash() {
+  location.hash = "#welcome";
+}
+
+document.querySelector("#expand").addEventListener("click", () => {
+  state.folded = !state.folded;
+  resetHash();
+});
+
+function onHashChange(event) {
+  const target = document.getElementById(location.hash.slice(1));
+  if (!target) return;
+  if (!state.folded) return;
+  for (const el of document.querySelectorAll(".fold")) {
+    if (el.contains(target)) {
+      state.folded = false;
+      target.scrollIntoView(true);
+      setExpand(false);
+    }
+  }
+}
+window.addEventListener("hashchange", onHashChange);
+window.addEventListener("DOMContentLoaded", onHashChange);
